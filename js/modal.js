b352b9e5d3eb806e5060cb52a75f29f551877812
Vlad Makhnenko <vlmakh@gmail.com>
2023-02-21 09:23:00 +0200

    update modal show

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,35 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const refs = {
+  openModalBtn: document.querySelector("[data-modal-open]"),
+  closeModalBtn: document.querySelector("[data-modal-close]"),
+  backdrop: document.querySelector("[data-backdrop]"),
+};
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+refs.openModalBtn.addEventListener("click", openModal);
+refs.closeModalBtn.addEventListener("click", closeModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+function openModal() {
+  refs.backdrop.classList.remove("is-hidden");
+
+  window.addEventListener("keydown", onEscKeyPress);
+  refs.backdrop.addEventListener("click", onBackDropClick);
+}
+
+function closeModal() {
+  refs.backdrop.classList.add("is-hidden");
+  refs.backdrop.removeEventListener("click", onBackDropClick);
+}
+
+function onEscKeyPress(event) {
+  if (event.code !== "Escape") {
+    return;
   }
-})();
+
+  window.removeEventListener("keydown", onEscKeyPress);
+  closeModal();
+}
+
+function onBackDropClick(event) {
+  if (event.currentTarget === event.target) {
+    closeModal();
+  }
+}
